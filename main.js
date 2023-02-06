7263a3c760672f7dcf92faa929f8ffc2dde6fcf9
Xystus96vic <116673658+Xystus96vic@users.noreply.github.com>
2023-02-06 17:05:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ function welcome(){
   let name = prompt('Hi, there what is your name?');
   
   switch (name) {
-    case "Xystus":
+    case "Naitoti":
       function toti() {alert('Hi there Zes, hope you like this website. Like how would you rate it? Feedback send it to one of my socialmedia platforms listed on the page. -This message has been programed to only be seen by Xystus.- Hahaha :-)');}
       break;
-   case "xystus ":
+   case "naitoti ":
       toti();
       break;
-    case "Xystus ":
+    case "Naitoti ":
       toti();
       break;
-    case "xystus":
+    case "naitoti":
       toti();
     break;
   }
